fa2700c0492f5e28fa080ea21baa5c2d8d89a994
MahlerFive <jefflegweak@gmail.com>
2013-11-14 01:02:50 -0500

    Adding start of assignment 1, part 5

---
+++ Assignment1-5/assignment1-5.py
@@ -0,0 +1,4 @@
+# Minimum Skew Problem: Find a position in a genome minimizing the skew.
+#      Input: A DNA string Genome.
+#      Output: All integer(s) i minimizing Skew(Prefixi (Text)) among all values of i (from 0 to |Genome|)
+
